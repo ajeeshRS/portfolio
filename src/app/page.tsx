1deecec8a360e577e7c22d8f7fb96269b820fc85
ajeeshRS <ajeeshrs79@gmail.com>
2025-07-15 23:09:08 +0530

    updated text content - Home page to SOON...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
-      <p>Home page</p>
+      <p className="text-3xl font-bold text-slate-400">SOON...</p>
     </div>
   );
 }
